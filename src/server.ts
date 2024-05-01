1a12d87eac6faa189f95816be6b5a0330d6cc87a
LockWard <roger1210@live.com>
2024-05-01 14:35:03 -0400

    Fixing the route of initialSetup.js

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
-import app from './index.js'
-import { PORT } from './config/config.js'
-import '../src/libs/inititalSetup.js' // This create the user admin and its role
+import app from './index.js';
+import { PORT } from './config/config.js';
+import './libs/inititalSetup.js'; // This create the user admin and its role
 
 app.listen(PORT, () => {
     console.log(`Listening on port...${PORT}`)
-})
+});
